aabb0052601a55080d47f6ace2b5fa7156e3c19d
Brian Jing <bjing@users.noreply.github.com>
2021-10-30 19:27:32 +1100

    Add missing arch fonts setup script

---
+++ arch/scripts/setup_fonts.sh
@@ -0,0 +1,6 @@
+# Set up terminal fonts, big and clear
+sudo pacman -Syu --noconfirm terminus-font
+setfont ter-124n -m 8859-2
+
+# Install Chinese fonts
+sudo pacman -Syu --noconfirm adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts
